2626901587c759f7bf717d83e3dffa7c8941dc81
Marcin Gryszkalis <mg@fork.pl>
2019-02-05 21:19:11 +0100

    new quick centroid based version

--- prepare-map-zgid-teryt.pl
+++ prepare-map-zgid-teryt.pl
@@ -1,17 +1,15 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use utf8;
 use Data::Dumper;
 use POSIX qw(strftime);
-use List::Util qw(shuffle);
 use Math::Polygon;
 
 use Geo::ShapeFile;
 use Geo::ShapeFile::Point;
 use Geo::Proj4;
 use Geo::Gpx;
-
-use LWP::Simple;
 
 my $proj = Geo::Proj4->new(init => "epsg:2180") or die;
 
@@ -29,51 +27,53 @@ while (<$mf>)
 }
 close ($mf);
 
+my $terytnaz;
+
 for my $zgid (1..2479)
 {
     next if exists $zgt->{$zgid};
 
     my $i = 1;
-    my $h = get("http://zaliczgmine.pl/communes/view/$zgid");
-    next unless $h =~ m/L.polygon/;
-    $h =~ s/.*L.polygon\((.*?)\).*/$1/s;
+
+    my $h = `curl -s "http://zaliczgmine.pl/communes/view/$zgid"`;
+
+    next unless $h =~ m/.*L.polygon\((.*?)\).bindLabel\('([^']+)'.*/s;
+    my $label = $2;
+    $h = $1;
     $h = eval $h;
     
-    # take random 100 points from border
-    my @hh = shuffle(@$h);
-    @hh = @hh[1..100]; 
-    $h = \@hh;
-
-#    my $poly = Math::Polygon->new();    
-
+    push(@$h,$h->[0]); # polygon must be closed
+    my $poly = Math::Polygon->new(@$h);    
+    my $pt = $poly->centroid;
+    
     my $m;
     
-    while (my $pt = pop @$h) 
+    my @p = $proj->forward($pt->[0], $pt->[1]);
+    
+    my @f = $shapefile->shapes_in_area($p[0], $p[1], $p[0], $p[1]);
+
+    my $ms = -1;
+    for my $id (@f)
     {
-        my @p = $proj->forward($pt->[0], $pt->[1]);
+        my $shape = $shapefile->get_shp_record($id);
+        my $point = Geo::ShapeFile::Point->new(X => $p[0], Y => $p[1]);
+        next unless $shape->contains_point($point);
     
-        my @f = $shapefile->shapes_in_area($p[0], $p[1], $p[0], $p[1]);
-    
-        for my $id (@f)
-        {
-            my $shape = $shapefile->get_shp_record($id);
-            my $point = Geo::ShapeFile::Point->new(X => $p[0], Y => $p[1]);
-            next unless $shape->contains_point($point);
-    
-            my %db = $shapefile->get_dbf_record($id);
-            printf "%4d %3d %6d %7s %s\n", $zgid, $i++, $id, $db{jpt_kod_je}, $db{jpt_nazwa_};
-    
-            $m->{$db{jpt_kod_je}}++;
-        }
-    
-#         last if $i > 20;
+        my %db = $shapefile->get_dbf_record($id);
+#        printf "%4d %3d %6d %7s %s\n", $zgid, $i++, $id, $db{jpt_kod_je}, $db{jpt_nazwa_};
+        $terytnaz->{$db{jpt_kod_je}} = $db{jpt_nazwa_};
+        $ms = $db{jpt_kod_je};
+
+        last;
     }
 
-    my @mss = sort { $m->{$b} <=> $m->{$a} } keys %$m;
-    # print STDERR Dumper $m;
-    
-    open(my $mf, ">>zgid-teryt-map.txt") or die $!;
-    print "$zgid:$mss[0]\n";
-    print $mf "$zgid:$mss[0]\n";
+    open($mf, ">>zgid-teryt-map.txt") or die $!;
+    my $status;
+    if ($label eq $terytnaz->{$ms}) { $status = 'OK' }
+    elsif ($label =~ m/^$terytnaz->{$ms}/) { $status = 'BG'; }
+    else { $status = '--'; }
+
+    print STDERR "$status $zgid ($label) -> $ms ($terytnaz->{$ms})\n";
+    print $mf "$zgid:$ms:$status:$label:$terytnaz->{$ms}\n";
     close($mf);
 }
