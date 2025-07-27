3d223b6c4bd1b06905cf6942b14925364c05a13a
Marcin Gryszkalis <mg@fork.pl>
2025-07-27 12:36:13 +0200

    parallel processing

--- process-all-gpxes.pl
+++ process-all-gpxes.pl
@@ -4,30 +4,59 @@ use warnings;
 use Data::Dumper;
 use POSIX qw(strftime);
 use FindBin qw($Bin);
+use Parallel::Loops;
 
 my $source_dir = "gpx";
 my $dest_dir = "reports";
- 
-while (<$source_dir/*.gpx>)
+
+my $maxProcs = 8;
+my $pl = Parallel::Loops->new($maxProcs);
+
+my %returnValues;
+
+$pl->share( \%returnValues );
+my @gpxs;
+
+for (<$source_dir/*.gpx>)
 {
     my $sn = $_;
     s/.gpx/.txt/;
     s/^$source_dir/$dest_dir/;
     my $dn = $_;
-    
+
     next if -f $dn and -s $dn;
 
+    push(@gpxs, $sn)
+}
+
+$pl->foreach(\@gpxs, sub {
+
+    my $sn = $_;
+    s/.gpx/.txt/;
+    s/^$source_dir/$dest_dir/;
+    my $dn = $_;
+
+    return if -f $dn and -s $dn;
+
     print STDERR "processing: $sn -> $dn\n";
+
+    my $res;
 
     open(my $sf, "-|", "$Bin/gpx2gminy.pl", $sn) or die $!;
     open(my $df, ">", $dn) or die $!;
 
-    while (<$sf>) 
-    { 
+    while (<$sf>)
+    {
         print $df $_;
-        print STDERR $_;
+        $res .= $_;
     }
 
     close($sf);
     close($df);
-}
+
+    print STDERR $res;
+
+#    $returnValues{$_} = sqrt($_);
+});
+
+
